80ef9b6c08c86b128082f0478fc3ece584235448
Arvid van de Crommert <arvidvdc@gmail.com>
2019-10-24 06:42:57 +0200

    Eerste functionaliteit toegevoegd

--- assets/js/lingo.js
+++ assets/js/lingo.js
@@ -1,1 +1,18 @@
-console.log("Connected")
+//variabelen defineren
+var gekozenWoord = "A";
+var opgegevenWoord;
+
+//eventlistners
+document.getElementById("ctrlLetter").addEventListener("click",function(){
+    // letter naar variabele
+    var opgegevenWoord = document.getElementsByTagName("input")[0].value.toUpperCase();
+    // letter contoleren
+    if(opgegevenWoord!=gekozenWoord) {
+        // letter fout 
+        console.log("Letter is fout.");
+    } else {
+        console.log("Letter is goed.");
+        // letter in woord
+        // letter op de juiste plaats
+    };
+});
