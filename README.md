9b3c7e66b19fa9998ea9cdc19160364889fa3bf9
Arvid van de Crommert <arvidvdc@gmail.com>
2019-10-25 01:10:07 +0200

    Aantal js routines, css en html toegevoegd

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # lingo
 Lingo woordspel
+
+Het is de bedoeling om het spel Lingo te maken met javascript, html en css.
+
+##Uitwerking
+- Het spel moet herkenbaar zijn als het bekende spel Lingo.
+- Het spel moet speelbaar zijn op een smartphone.
+- De opbouw moet via javascript, html en css  vormgegeven worden.
+
+##Spelverloop
+01 Kiezen 1 (even of oneven) of 2 spelers
+02 Woord kiezen
+03 Cijferkaart indelen
+04 Cijfers wegstrepen
+05 Letters invoeren
+06 Woord controle
+07a Indien woord goed:
+        ballenbak
+        Groene bal --> naar geheugen
+        Rode bal --> beurt over
+        Cijfer bal --> cijfer afstrepen
+        Max ballen --> beurt over
+07b Indien woord fout:
+        Beurt over.
+08 Indien Lingo: einde spel.
+00 Overname beurt
+
+##Aanvullende ideeen
+- Er moet de mogelijkheid geboden worden om te kiezen tussen 1 of 2 spelers.
--- assets/js/lingo.js
+++ assets/js/lingo.js
@@ -1,18 +1,44 @@
 //variabelen defineren
-var gekozenWoord = "A";
-var opgegevenWoord;
+var gekozenWoord = ["A","B","C","D","E"];
+var opgegevenWoord = [];
+var arrayWoorden = ["Water","Woord","Groen","Jacht","Graaf","Fiets","Trein","Wagen","Boord","Braam","Vrouw","Nooit","Groot","Brood","Drank","Braaf","Draag","Liefde","Paars","Waard","Plaag","Paard","Baard","Drugs","Bende","Zwart","Schot","Schop","Bench","Trouw","Ander","Check"]
+
+// Bij laden van de pagina direct focus naar eerste input element.
+document.getElementsByTagName("input")[0].focus();
 
 //eventlistners
+
+//controle letter
 document.getElementById("ctrlLetter").addEventListener("click",function(){
-    // letter naar variabele
-    var opgegevenWoord = document.getElementsByTagName("input")[0].value.toUpperCase();
-    // letter contoleren
-    if(opgegevenWoord!=gekozenWoord) {
-        // letter fout 
-        console.log("Letter is fout.");
-    } else {
-        console.log("Letter is goed.");
-        // letter in woord
-        // letter op de juiste plaats
+    // letters naar array
+    for(i=0;i<5;i++) {
+        opgegevenWoord.push(document.getElementsByTagName("input")[i].value.toUpperCase());
     };
+
+    // woord contoleren
+    for(i=0;i<5;i++){
+        if(opgegevenWoord[i]!=gekozenWoord[i]){
+            // Als letter in woord, dan class toekennen anders niets doen
+            console.log(gekozenWoord)
+            if(gekozenWoord.indexOf(opgegevenWoord[i]==-1)){
+                console.log("Letter NIET in het woord.");
+                document.getElementsByTagName("input")[i].classList.add("letterNormaal");
+            } else {
+                console.log("Letter in het woord.");
+                document.getElementsByTagName("input")[i].classList.add("letterInWoord");
+            }
+        } else if(opgegevenWoord[i]==gekozenWoord[i]){
+            // letter in woord
+            console.log("Letter op de juiste plaats.");
+            document.getElementsByTagName("input")[i].classList.add("letterGoed");
+        } else {
+            // letter op de juiste plaats
+            console/log("ELSE")
+        };
+    };
+});
+
+// naar de tweede letter gaan.
+document.getElementsByTagName("input")[0].addEventListener("input",function(){
+    document.getElementsByTagName("input")[1].focus();
 });
