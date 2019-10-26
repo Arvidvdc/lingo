1576e51fb4c794de8ad476fad861c0273a00bd24
Arvid van de Crommert <arvidvdc@gmail.com>
2019-10-26 15:27:51 +0200

    Trello 20: css toegepast op 1e letter

--- assets/js/lingo.js
+++ assets/js/lingo.js
@@ -6,43 +6,47 @@ var arrayWoorden = [
 var gekozenWoord = [];
 
 //Kiezen willekeurig woord
-var tijdelijkWoord = woordKiezer();
+// var tijdelijkWoord = woordKiezer();
+var tijdelijkWoord="woord"
 for(var i=0;i<tijdelijkWoord.length;i++){
     gekozenWoord.push(tijdelijkWoord.split("")[i].toUpperCase());
 }
+// Eerste letter toevoegen aan speelbord
 document.getElementsByTagName("input")[0].value=gekozenWoord[0];
+document.getElementsByTagName("input")[0].classList.add("letterGoed");
 document.getElementsByTagName("input")[1].focus();
-console.log("Gekozen woord: "+gekozenWoord);
 
-//Controle letter
+
+//console.log("Gekozen woord: "+gekozenWoord);
+
+//Eventlistener(s) opgeven
 document.getElementById("ctrlLetter").addEventListener("click",function(){
     // Gekozen letters naar array
-    var opgegevenWoord = [];
+    var ingevoerdWoord = [];
     for(i=0;i<5;i++) {
-        opgegevenWoord.push(document.getElementsByTagName("input")[i].value.toUpperCase());
+        ingevoerdWoord.push(document.getElementsByTagName("input")[i].value.toUpperCase());
     };
 
     // Woord contoleren
     for(i=0;i<5;i++){
-        var opgegevenLetter = opgegevenWoord[i]
-        if(opgegevenLetter != gekozenWoord[i]){
+        var ingevoerdeLetter = ingevoerdWoord[i]
+        if(ingevoerdeLetter != gekozenWoord[i]){
             // Letter niet overeenkomend. Verdere controle
             for(var j=1;j<5;j++) {
                 console.log("i= " +i +" - " +"j= " +j);
-                console.log(opgegevenLetter +" - " +gekozenWoord[j]);
+                console.log(ingevoerdeLetter +" - " +gekozenWoord[j]);
                 // console.log("Letter in het woord.");
-                if(opgegevenLetter == gekozenWoord[j]) {
+                if(ingevoerdeLetter == gekozenWoord[j]) {
                     // Letter wel in het woord, niet de juiste plaats
                     document.getElementsByTagName("input")[i].classList.add("letterInWoord");
-                    // Letter wel in het woord maar letter reeds goed gegeven: gekozenWoord=groen / opgegevenWoord= grEep (1e E)
-                    // Letter wel in het woord maar letter komt vaker voor: gekozenWoord=woord / opgegevenWoord= bOten
+                    // Letter wel in het woord maar letter reeds goed gegeven: gekozenWoord=groen / ingevoerdWoord= grEep (1e E)
+                    // Letter wel in het woord maar letter komt vaker voor: gekozenWoord=woord / ingevoerdWoord= bOten
                 } else {
                     // Letter niet in het woord
                     document.getElementsByTagName("input")[i].classList.add("letterNormaal");
                 };
-                
             };
-        } else if(opgegevenLetter == gekozenWoord[i]){
+        } else if(ingevoerdeLetter == gekozenWoord[i]){
             // Letter op de juiste plaats
             document.getElementsByTagName("input")[i].classList.add("letterGoed");
         };
